c1f625db807e80faae17538fb24f97d822432d84
MartaTonet <martadaconceicaotonet@gmail.com>
2021-10-12 17:08:17 -0300

    adicionei arquivo usando table

---
+++ Atividades_Javascript/atividada3.js
@@ -0,0 +1,36 @@
+
+var dadosDoProcesso = [{
+    nome: "Antonica",
+    sobreNome: "Alberto",
+    idade: 63,
+    altura: 1.50,
+    nacionalidade: "Angola",
+} ,
+{
+    nome: "Arleth",
+    sobreNome: "Nandundu",
+    idade: 45,
+    altura: 1.65,
+    nacionalidade: "Brasil",
+},
+{
+    nome: "Rosa",
+    sobreNome: "Fatima",
+    idade: 25,
+    altura: 1.70,
+    nacionalidade: "Angola",
+},
+{
+    nome: "Fernando",
+    sobreNome: "Ribeiro",
+    idade: 30,
+    altura: 1.80,
+    nacionalidade: "venezuela",
+}]
+ 
+
+console.table(dadosDoProcesso); 
+
+
+
+
